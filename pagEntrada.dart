5929889049b635eb4344821f656ad89e76b59871
Moobiid <111130621+Moobiid@users.noreply.github.com>
2023-10-23 23:58:07 -0300

    Update pagEntrada.dart

--- pagEntrada.dart
+++ pagEntrada.dart
@@ -9,16 +9,29 @@ class PagEntrada extends StatefulWidget {
   State<PagEntrada> createState() => _PagEntradaState();
 }
 
+//Criando um controlador para que o programa reconheça o que o usuario digitou
+TextEditingController controlador = TextEditingController();
+
 class _PagEntradaState extends State<PagEntrada> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 85, 85, 85),
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("Página de Login"),
-        backgroundColor: Color.fromARGB(255, 0, 0, 0),
-      ),
-    );
+        backgroundColor: const Color.fromARGB(255, 85, 85, 85),
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Página de Login"),
+          backgroundColor: Color.fromARGB(255, 0, 0, 0),
+        ),
+        body: Center(
+          child: Container(
+            height: 150,
+            width: 300,
+            color: const Color.fromARGB(255, 114, 114, 114),
+            child: TextField(
+              controller: controlador,
+              decoration: InputDecoration(),
+            ),
+          ),
+        ));
   }
 }
