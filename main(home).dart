a51d5955347d5118afd7b56a43ea4ac5461e7439
Moobiid <111130621+Moobiid@users.noreply.github.com>
2023-10-23 23:23:28 -0300

    Update main(home).dart

--- main(home).dart
+++ main(home).dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:app_01/projeto/pagInicial.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 
@@ -17,36 +18,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 85, 85, 85),
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text("Projeto Aula"),
-          backgroundColor: Color.fromARGB(255, 0, 0, 0),
-        ),
-        body: Center(
-          child: OutlinedButton(
-            onPressed: () {}, // Perform an action when the button is pressed
-            style: OutlinedButton.styleFrom(
-              minimumSize: Size(200, 50),
-              fixedSize: Size(200, 50),
-              backgroundColor: const Color.fromARGB(255, 90, 90, 90),
-              side: const BorderSide(
-                  color: Color.fromARGB(255, 134, 134, 134), width: 2),
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            ),
-
-            child: const Text(
-              'Start',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 245, 246, 247),
-              ),
-            ),
-          ),
-        ),
-      ),
+      home: PagLogin(),
     );
   }
 }
