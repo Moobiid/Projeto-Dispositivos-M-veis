5aded6df57101135501acc375c3c6982bfc3faf5
Moobiid <111130621+Moobiid@users.noreply.github.com>
2023-10-24 21:14:04 -0300

    Update pagEntrada.dart
    Mudança para uma Column e adição de dois widgets de textfields, para inserção de e-mail e senha, com um botão para exibir e ocultar a senha

--- pagEntrada.dart
+++ pagEntrada.dart
@@ -10,7 +10,9 @@ class PagEntrada extends StatefulWidget {
 }
 
 //Criando um controlador para que o programa reconheça o que o usuario digitou
-TextEditingController controlador = TextEditingController();
+TextEditingController controladorEmail = TextEditingController();
+TextEditingController controladorSenha = TextEditingController();
+bool _senhaVisivel = false;
 
 class _PagEntradaState extends State<PagEntrada> {
   @override
@@ -23,29 +25,79 @@ class _PagEntradaState extends State<PagEntrada> {
           backgroundColor: Color.fromARGB(255, 0, 0, 0),
         ),
         body: Center(
-          child: Container(
-            height: 150,
-            width: 300,
-            child: TextField(
-              controller: controlador,
-              decoration: InputDecoration(
-                labelText: "E-mail",
-                labelStyle: TextStyle(
-                    color: Color.fromARGB(255, 233, 233, 233),
-                    fontWeight: FontWeight.bold),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    borderSide: BorderSide(color: Colors.black)),
-                disabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
-                  borderSide: BorderSide(color: Colors.black),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
-                  borderSide: BorderSide(color: Colors.black),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //Container criado para definir o textfield de e-mail
+              Container(
+                height: 55,
+                width: 300,
+                child: TextField(
+                  controller: controladorEmail,
+                  decoration: InputDecoration(
+                    labelText: "E-mail",
+                    labelStyle: TextStyle(
+                        color: Color.fromARGB(255, 233, 233, 233),
+                        fontWeight: FontWeight.bold),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: BorderSide(color: Colors.black)),
+                    disabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                  ),
                 ),
               ),
-            ),
+
+              Container(
+                height: 150,
+                width: 300,
+                child: TextField(
+                  //Fazendo com que a senha se torne visivel de forma dinamica atraves da declarção booleana "!""
+                  obscureText: !_senhaVisivel,
+                  controller: controladorSenha,
+                  decoration: InputDecoration(
+                    labelText: "Senha",
+                    labelStyle: TextStyle(
+                        color: Color.fromARGB(255, 233, 233, 233),
+                        fontWeight: FontWeight.bold),
+
+                    //Criando um botao para mostrar/ocultar a senha
+                    suffixIcon: IconButton(
+                        splashRadius: 20,
+                        onPressed: () {
+                          //Atualiza o estado da variavel senhaVisivel (true/false)
+                          setState(() {
+                            _senhaVisivel = !_senhaVisivel;
+                          });
+                        },
+                        icon: Icon(
+                          _senhaVisivel
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                          color: Colors.black26,
+                        )),
+
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: BorderSide(color: Colors.black)),
+                    disabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                  ),
+                ),
+              )
+            ],
           ),
         ));
   }
