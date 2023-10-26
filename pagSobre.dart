36ebf74800862e8bb45375c81c3f13f0aaa78af2
Moobiid <111130621+Moobiid@users.noreply.github.com>
2023-10-26 03:27:53 -0300

    Update pagSobre.dart

--- pagSobre.dart
+++ pagSobre.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:app_01/projeto/pagEntrada.dart';
 
 class pagSobre extends StatefulWidget {
   const pagSobre({super.key});
@@ -15,10 +16,44 @@ class _pagSobreState extends State<pagSobre> {
         home: Scaffold(
           backgroundColor: Color.fromARGB(255, 102, 102, 102),
           appBar: AppBar(
+            leading: BackButton(
+              onPressed: () {
+                Navigator.pop(context,
+                    MaterialPageRoute(builder: (context) => PagEntrada()));
+              },
+            ),
             centerTitle: true,
             // ignore: prefer_const_constructors
             title: Text("Sobre"),
             backgroundColor: Color.fromARGB(255, 0, 0, 0),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Tema: Desenvolver um aplicativo de venda de Tênis',
+                  style: TextStyle(color: Colors.white),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Objetivo: Fazer com que o usuário veja os produtos disponiveis e escolha comprar ou não',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 10,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Desenvolvedor: João Victor Mobiglia Podenciano',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
           ),
         ));
   }
